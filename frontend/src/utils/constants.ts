a37678de18ed1fe627b92d7ad22cbe3f5ced657d
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-08-25 12:59:39 +0545

    Backend API Changes

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,3 +1,3 @@
-export const USER_API_ENDPOINT = "http://localhost:8080/api/v1/user";
+export const USER_API_ENDPOINT = "https://blog-backend-wzfq.onrender.com/api/v1/user";
 
-export const BLOG_API_ENDPOINT = "http://localhost:8080/api/v1/blog";
+export const BLOG_API_ENDPOINT = "https://blog-backend-wzfq.onrender.com/api/v1/blog";
